93991c33db94f29349db6d693dda68c35921df74
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-02-11 13:59:57 +0300

    Update run.py
    DB Stuff

--- codebase/run.py
+++ codebase/run.py
@@ -6,6 +6,42 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dripBase.db'
 db = SQLAlchemy(app)
 
+
+class product(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(40), nullable=False)
+	picture = db.Column(db.String(80),unique=True, nullable=False)
+	category = db.Column(db.String(20), nullable=False)
+	rating = db.Column(db.Integer)
+	date = db.Column(db.String(20),nullable=False)
+	quantity = db.Column(db.Integer,nullable=False)
+
+class brand(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(40), nullable=False)
+	logo = db.Column(db.String(80),unique=True)
+	rating = db.Column(db.Integer)
+
+class attributes(db.Model):
+	id = db.Column(db.Integer,primary_key=False)
+	gender = db.Column(db.String(10), nullable=False)
+	size = db.Column(db.String(10), nullable=False)
+	color = db.Column(db.String(20), nullable=False)
+
+class pictures(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	path = db.Column(db.String(100), nullable=False)	
+
+class ratings(db.Model):
+	id = db.Column(db.Integer,primary_key=False)
+	user = db.Column(db.String(100), nullable=False)
+	rating = db.Column(db.Integer, nullable=False)
+	review = db.Column(db.String(200), nullable=False)
+	seller = db.Column(db.Integer, nullable=False)
+	brand = db.Column(db.Integer, nullable=False)
+	
+
+
 @app.route('/')
 def home():
 	return render_template('homepage.html')
