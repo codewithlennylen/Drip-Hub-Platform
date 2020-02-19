ec5e05c86922afc559112bd73ec791a0be635010
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-02-19 23:11:23 +0300

    Database Migrations Implementation
    The first test seems to work just fine. However, I am not yet satisfied.

--- codebase/config.py
+++ codebase/config.py
@@ -1,4 +1,12 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-nevr-guess'
+
+	#---
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///'+ os.path.join(basedir,'dripBase.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
