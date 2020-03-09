2af67f443501051878d0def186fa1b4c4ff52ce6
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-03-09 16:39:53 +0300

    Gen.Products Page
    All seems to be working well. However, i need to generate more data to work with. Much more data is required.
    
    But before then, i have to set a few standards regarding the CMS for a more productive workflow in the future.

--- codebase/app/routes.py
+++ codebase/app/routes.py
@@ -2,13 +2,26 @@ from flask import render_template
 from app import app
 from app.models import *
 
+
+main_cards = ['mfashion','ffashion','kfashion','narrivals']
+
 @app.route('/')
 @app.route('/index/')
 def index():
+	# Get all categories in the database categories table.
+	# * main_cards aren't part of this query
 	c = category.query.all()
 	return render_template('homepage.html', c = c)
 
 
-@app.route('/gen/')
-def general():
-	return render_template('genproducts.html')
+@app.route('/gen/<string:category_name>/')
+def general(category_name):
+	if category_name in main_cards: # Different filter for the main_cards
+		pass
+	else:							# Get all products(+ details) in category x
+		cat = category.query.filter_by(categoryName = category_name).first()
+		catID = cat.id
+
+		prods = products.query.filter_by(categoryid = catID).all()
+
+	return render_template('genproducts.html', prods=prods)
