faf9fd8f2984b3b73ca3dab051af62adfdfc0a68
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-06-22 13:14:50 +0300

    App Structure Shift Successful :100:
    Structure shifted to Julian Nash Style

--- codebase/app/__init__.py
+++ codebase/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_pyfile('config.py')
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+
+from app import store_views, models
---
+++ codebase/app/config.py
@@ -0,0 +1,4 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/dripbasetest' # Dripbase test doesn't use rating-products relationship
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = 'Youw!lln3v3rgu3$$'
