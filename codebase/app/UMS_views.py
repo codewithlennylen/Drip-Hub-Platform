d73ef46563ff26d7d035f841e757911b1b172f14
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-07-13 23:21:25 +0300

    Added Login Page / Form
    LoginForm Class > forms.py
    login.html
    login route  > UMS_views.py

--- codebase/app/UMS_views.py
+++ codebase/app/UMS_views.py
@@ -1,3 +1,10 @@
 from flask import render_template
 from app import app
 from app.models import *
+from app.forms import LoginForm
+
+
+@app.route('/login/')
+def login():
+	form = LoginForm()
+	return render_template('UMS_templates/login.html', form = form)
--- codebase/app/config.py
+++ codebase/app/config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/dripbasetest' # Dripbase test doesn't use rating-products relationship
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = 'Youw!lln3v3rgu3$$'
+SECRET_KEY = 'd1e3e44cd8382ec0a033a014102d74b4bf0025593475733db02efc0432deb38e'
