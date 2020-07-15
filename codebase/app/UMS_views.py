617154461765aaee86b27abf06434addd0b58d05
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-07-15 22:31:33 +0300

    Register Form + Nav-link update
    Register Form  + Register Route Created
    Nav-Link pointing to Sign In page also updated

--- codebase/app/UMS_views.py
+++ codebase/app/UMS_views.py
@@ -1,10 +1,15 @@
 from flask import render_template
 from app import app
 from app.models import *
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET','POST'])
 def login():
 	form = LoginForm()
 	return render_template('UMS_templates/login.html', form = form)
+
+@app.route('/register/', methods=['GET','POST'])
+def register():
+	form = RegisterForm()
+	return render_template('UMS_templates/register.html', form = form)
--- codebase/app/forms.py
+++ codebase/app/forms.py
@@ -3,7 +3,13 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email Address', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Register')
