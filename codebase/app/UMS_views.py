fdfb0bb66e31e09aa90427f2fa66f96aab43b91c
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-07-15 23:45:43 +0300

    Abandoning Flask-WTForms > Bootstrap Forms
    I initially thought that I could customize Flask-WTForms but after trial and error, I have failed. Thus I am switching to Bootstrap Forms, which have worked out for me in the past.
    
    Flask-wtf however, does offer global protection of the Flask app against CSRF! (which I initially thought was only possible by using Flask-wtf forms!)

--- codebase/app/UMS_views.py
+++ codebase/app/UMS_views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
 from app.models import *
 from app.forms import LoginForm, RegisterForm
@@ -7,9 +7,19 @@ from app.forms import LoginForm, RegisterForm
 @app.route('/login/', methods=['GET','POST'])
 def login():
 	form = LoginForm()
+
+	if form.validate_on_submit():
+		flash(f'Successfully Signed In as {form.email.data}')
+		return redirect(url_for("index"))
+
 	return render_template('UMS_templates/login.html', form = form)
 
 @app.route('/register/', methods=['GET','POST'])
 def register():
 	form = RegisterForm()
+
+	if form.validate_on_submit():
+		flash(f'Successfully Registered as {form.email.data}')
+		return redirect(url_for("index"))
+
 	return render_template('UMS_templates/register.html', form = form)
--- codebase/app/__init__.py
+++ codebase/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
