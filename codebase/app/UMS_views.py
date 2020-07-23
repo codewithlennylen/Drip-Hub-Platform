86d3c76f0be774420f56724418c5ffbdfdb8bdcc
codewithlennylen <33583893+codewithlennylen@users.noreply.github.com>
2020-07-23 22:10:21 +0300

    More on Forms

--- codebase/app/UMS_views.py
+++ codebase/app/UMS_views.py
@@ -1,25 +1,47 @@
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 from app import app
 from app.models import *
-from app.forms import LoginForm, RegisterForm
+# from app.forms import LoginForm, RegisterForm
 
 
 @app.route('/login/', methods=['GET','POST'])
 def login():
-	form = LoginForm()
+	# form = LoginForm()
 
-	if form.validate_on_submit():
-		flash(f'Successfully Signed In as {form.email.data}')
+	if request.method == 'POST':
+		req = request.form
+
+		missing = list()
+		for k, v in req.items():
+			if v == "":
+				missing.append(k)
+
+		if missing:
+			flash("Please Fill in All the Fields")
+			return render_template("UMS_templates/login.html")
+
+		flash(f'Successfully Signed In as {request.form["inputEmail"]}:{request.form["inputPassword"]}')
 		return redirect(url_for("index"))
 
-	return render_template('UMS_templates/login.html', form = form)
+	return render_template('UMS_templates/login.html')
 
 @app.route('/register/', methods=['GET','POST'])
 def register():
-	form = RegisterForm()
+	# form = RegisterForm()
 
-	if form.validate_on_submit():
-		flash(f'Successfully Registered as {form.email.data}')
+	if request.method == 'POST':
+		req = request.form
+
+		missing = list()
+		for k, v in req.items():
+			if v == "":
+				missing.append(k)
+
+		if missing:
+			flash("Please Fill in All the Fields")
+			return render_template("UMS_templates/register.html")
+			
+		flash(f'Successfully Registered as {request.form["inputFname"]}:{request.form["inputPhone"]}')
 		return redirect(url_for("index"))
 
-	return render_template('UMS_templates/register.html', form = form)
+	return render_template('UMS_templates/register.html')
