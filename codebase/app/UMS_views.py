4130a031d21aaae1c122477101f38fcb62a927c0
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-07-29 15:53:24 +0300

    Register Backend :+1:
    The Register Functionality of the User-Management System Works like a charm

--- codebase/app/UMS_views.py
+++ codebase/app/UMS_views.py
@@ -1,20 +1,35 @@
 from flask import render_template, redirect, flash, url_for, request
-from app import app
+from app import app, db
 from app.models import *
-# from app.forms import LoginForm, RegisterForm
+
+
+def register_user(f_name, l_name, p_number, e_address, pwd):
+	customer = customers(fname=f_name,
+                      lname=l_name,
+                      phone1=p_number,
+                      email=e_address,
+                      pword=pwd)
+
+	print(customer.fname)
+	
+	try:
+		db.session.add(customer)
+		db.session.commit()
+	except Exception as e:
+		print(e)
+	
 
 
 @app.route('/login/', methods=['GET','POST'])
 def login():
-	# form = LoginForm()
-
 	if request.method == 'POST':
 		req = request.form
 
-		missing = list()
-		for k, v in req.items():
-			if v == "":
-				missing.append(k)
+		# Server-side Data Validation
+		missing = False
+		for _, v in req.items():
+			if v == "":  # Checking for any Missing Fields
+				missing = True
 
 		if missing:
 			flash("Please Fill in All the Fields")
@@ -27,20 +42,31 @@ def login():
 
 @app.route('/register/', methods=['GET','POST'])
 def register():
-	# form = RegisterForm()
-
 	if request.method == 'POST':
 		req = request.form
 
-		missing = list()
-		for k, v in req.items():
-			if v == "":
-				missing.append(k)
+		# Server-side Data Validation
+		missing = False
+		for _, v in req.items():
+			if v == "": # Checking for any Missing Fields
+				missing = True 
 
 		if missing:
 			flash("Please Fill in All the Fields")
 			return render_template("UMS_templates/register.html")
-			
+		
+		# Proceed to Register the User by adding them to the Database
+		first_name = req['inputFname']
+		last_name = req['inputLname']
+		phone_number = req['inputPhone']
+		email_address = req['inputEmail']
+		pasword = req['inputPassword']
+
+		# Verify the Input - Password,names, email? >> Bootstrap Client-Side Validation ?
+
+		# Pass the Fields to the register_user() Function
+		register_user(first_name, last_name, phone_number, email_address, pasword)
+
 		flash(f'Successfully Registered as {request.form["inputFname"]}:{request.form["inputPhone"]}')
 		return redirect(url_for("index"))
 
